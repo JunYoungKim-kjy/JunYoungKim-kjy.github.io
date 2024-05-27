c59f9bc80fe641ac61fb14c19ab3bfbdcc4f23a2
JunYoungKim-kjy <145628089+JunYoungKim-kjy@users.noreply.github.com>
2024-05-27 15:49:58 +0900

    update

--- resources/script/index.js
+++ resources/script/index.js
@@ -1,43 +1,43 @@
-// const headers = [...document.getElementsByTagName("header")];
-//
-// headers.forEach((header) => {
-//   const tags = header.getElementsByClassName("tag");
-//   const projectTag = tags[0];
-//   const myInfosTag = tags[1];
-//
-//   const mySections = document.querySelectorAll(".showTags");
-//   mySections.forEach((section) => {
-//     if (section.dataset.value === "projects") {
-//       section.addEventListener("mouseenter", (e) => {
-//         projectTag.setAttribute("style", "display : block");
-//       });
-//
-//       section.addEventListener("mouseleave", (e) => {
-//         projectTag.setAttribute("style", "display : none");
-//       });
-//     } else {
-//       section.addEventListener("mouseenter", (e) => {
-//         myInfosTag.setAttribute("style", "display : block");
-//       });
-//
-//       section.addEventListener("mouseleave", (e) => {
-//         myInfosTag.setAttribute("style", "display : none");
-//       });
-//     }
-//   });
-// });
-//
-// const backendDiv = document.querySelector(".backend");
-// const frontendDiv = document.querySelector(".frontend");
-//
-// function Project(link, imageUrl, title, description, skills, period) {
-//   this.link = link;
-//   this.imageUrl = imageUrl;
-//   this.title = title;
-//   this.description = description;
-//   this.skills = skills;
-//   this.period = period;
-// }
+const headers = [...document.getElementsByTagName("header")];
+
+headers.forEach((header) => {
+  const tags = header.getElementsByClassName("tag");
+  const projectTag = tags[0];
+  const myInfosTag = tags[1];
+
+  const mySections = document.querySelectorAll(".showTags");
+  mySections.forEach((section) => {
+    if (section.dataset.value === "projects") {
+      section.addEventListener("mouseenter", (e) => {
+        projectTag.setAttribute("style", "display : block");
+      });
+
+      section.addEventListener("mouseleave", (e) => {
+        projectTag.setAttribute("style", "display : none");
+      });
+    } else {
+      section.addEventListener("mouseenter", (e) => {
+        myInfosTag.setAttribute("style", "display : block");
+      });
+
+      section.addEventListener("mouseleave", (e) => {
+        myInfosTag.setAttribute("style", "display : none");
+      });
+    }
+  });
+});
+
+const backendDiv = document.querySelector(".backend");
+const frontendDiv = document.querySelector(".frontend");
+
+function Project(link, imageUrl, title, description, skills, period) {
+  this.link = link;
+  this.imageUrl = imageUrl;
+  this.title = title;
+  this.description = description;
+  this.skills = skills;
+  this.period = period;
+}
 
 function addProject(project, targetContainer, imageSize, imagePosition) {
   let container;
@@ -95,4 +95,4 @@ const bookSearch = new Project(
   ["2024.04.24", "2024.04.24"]
 );
 
-addProject(bookSearch, "frontend", "contain", "center");
+// addProject(bookSearch, "frontend", "contain", "center");
